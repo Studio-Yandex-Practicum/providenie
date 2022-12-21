20c6996d31b47db97d47a1ca747d3d64cdbd7b67
Андрей Распутин <rasputin21@gmail.com>
2022-12-21 23:01:38 +0500

    Change project structure, add run_polling

--- src/main.py
+++ src/main.py
@@ -1,23 +1,12 @@
-import logging
-import os
-
-from dotenv import load_dotenv
 from telegram.ext import Application
 
-
-load_dotenv()
-token = os.getenv('BOT_TOKEN')
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
-logger = logging.getLogger(__name__)
+import core.logger  # noqa
+from core.settings import TELEGRAM_TOKEN
 
 
 def main() -> None:
     """Run the bot."""
-    application = Application.builder().token(token).build()
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
 
     application.run_polling()
 
