76cd53e904ae3e41c576504290dcdc94b0017bd1
Николай Данилов <danilov.nikola2018j@yandex.ru>
2022-12-22 17:59:11 +0300

    Импорт settins / удаление тестовых перемсенных / добавлен удачный лог

--- core/email.py
+++ core/email.py
@@ -4,18 +4,16 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from .logger import logger
-from .settings import (
-    PORT_SMTP_SERVER,
-    SMTP_SERVER,
-    EMAIL_BOT,
-    EMAIL_BOT_PASSWORD,
-    EMAIL_CURATOR
-)
+from . import settings
 
+
+PORT_SMTP_SERVER = 587
+SMTP_SERVER = 'smtp.yandex.ru'
 
 ERROR_CANT_SEND_MSG_TO_EMAIL = 'Неудается отправить сообщение на email куратора!'
+SUCCESSFUL_SENDING_MSG = 'Сообщение отправлено куратору.'
 
-# тестовый шаблон. СДелай со мной что-нибудь.
+# Исправится, скорее всего, на этапе сбора информации от пользователя и отправке куратору
 HTML_TEMPLATE = """
     <html>
         <body>
@@ -25,9 +23,8 @@ HTML_TEMPLATE = """
     </html>
 """
 
-# ИСПАРВЬ МЕНЯ, КАК ТОЛЬКО БОТ ОЖИВЕТ И ПОЛУЧИТ СООБЩЕНИЕ ОТ ПОЛЬЗОВАТЕЛЯ
-# УБРАТЬ ТЕСТОВЫЕ ТЕМУ СООБЩЕНИЯ И ТЕКСТ. РАСКОММЕНТИТЬ subject, text_from_user
-def bot_send_email_to_curator(subject: str, text_from_user: str):
+
+def bot_send_email_to_curator(subject: str, text_from_user: str) -> bool:
     """
     Отправка ботом сообщение на email куратору.
     Subject - тема сообщения.
@@ -42,18 +39,17 @@ def bot_send_email_to_curator(subject: str, text_from_user: str):
         smtp_server.starttls()
 
         message = MIMEMultipart()
-        message['From'] = EMAIL_BOT
-        message['To'] = EMAIL_CURATOR
-        message['Subject'] = "Тестовая тема письма"
-        # message['Subject'] = subject
+        message['From'] = settings.EMAIL_BOT
+        message['To'] = settings.EMAIL_CURATOR
+        message['Subject'] = subject
 
-        smtp_server.login(EMAIL_BOT, EMAIL_BOT_PASSWORD)
+        smtp_server.login(settings.EMAIL_BOT, settings.EMAIL_BOT_PASSWORD)
 
-        message.attach(MIMEText("Текст вымышленного письма."))
-        # message.attach(MIMEText(text_from_user))
+        message.attach(MIMEText(text_from_user))
         message.attach(MIMEText(HTML_TEMPLATE, 'html'))
 
         smtp_server.send_message(message)
+        logger.info(SUCCESSFUL_SENDING_MSG)
 
         return True
 
