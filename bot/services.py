c467ff35e40a906c322a6393e9ae1127e639963c
Alexander Kuryatnikov <asander1995@gmail.com>
2022-12-22 03:05:45 +0300

    reply_message: added exceptions

--- bot/services.py
+++ bot/services.py
@@ -1,11 +1,17 @@
-from telegram import ReplyKeyboardMarkup, Update
+from telegram import InlineKeyboardMarkup, Update
+from telegram.error import Forbidden, TelegramError
+
+from core.logger import logger
 
 
 async def reply_message(
-        update: Update,
-        text: str,
-        reply_markup: ReplyKeyboardMarkup
+    update: Update, text: str, reply_markup: InlineKeyboardMarkup
 ) -> None:
     """Функция отправляет сообщение в Telegram чат."""
-    message = update.message
-    await message.reply_markdown_v2(text=text, reply_markup=reply_markup)
+    try:
+        message = update.message
+        await message.reply_markdown_v2(text=text, reply_markup=reply_markup)
+    except Forbidden:
+        logger.error("Бот заблокирован пользователем")
+    except TelegramError:
+        logger.error("Ошибка при ответе на сообщение")
