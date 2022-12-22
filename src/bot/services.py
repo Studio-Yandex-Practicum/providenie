9865ad0573b16a8df45180ace85749245a8c5545
Николай Данилов <danilov.nikola2018j@yandex.ru>
2022-12-22 18:32:26 +0300

    Отправка обьекта бота в main.py

--- src/bot/services.py
+++ src/bot/services.py
@@ -1,11 +1,12 @@
-from conversations.menu import start
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder
 
 from src.core.settings import TELEGRAM_TOKEN
 
 
 def start_bot():
-    """Функция инициализации и запуска бота."""
+    """
+    Функция инициализации бота.
+    Отправляет объект бота в main.py.
+    """
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-    start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
+    return application
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+from telegram.ext import CommandHandler
+
 import core.logger  # noqa
+from bot.conversations.menu import start
+from bot.services import start_bot
 
 
 def main() -> None:
-    pass
-
+    application = start_bot()
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+    
 
 if __name__ == "__main__":
     main()
