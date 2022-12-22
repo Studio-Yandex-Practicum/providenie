28f39d83b633fbe25145038b9cd412b713cf376d
Alexander Kuryatnikov <asander1995@gmail.com>
2022-12-22 18:01:21 +0300

    reply-message: directory change

--- src/bot/services.py
+++ src/bot/services.py
@@ -1,6 +1,9 @@
 from conversations.menu import start
+from telegram import InlineKeyboardMarkup, Update
+from telegram.error import Forbidden, TelegramError
 from telegram.ext import ApplicationBuilder, CommandHandler
 
+from src.core.logger import logger
 from src.core.settings import TELEGRAM_TOKEN
 
 
@@ -9,3 +12,16 @@ def start_bot():
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
+
+
+async def reply_message(
+    update: Update, text: str, reply_markup: InlineKeyboardMarkup
+) -> None:
+    """Функция отправляет сообщение в Telegram чат."""
+    try:
+        message = update.message
+        await message.reply_markdown_v2(text=text, reply_markup=reply_markup)
+    except Forbidden:
+        logger.error("Бот заблокирован пользователем")
+    except TelegramError:
+        logger.error("Ошибка при ответе на сообщение")
