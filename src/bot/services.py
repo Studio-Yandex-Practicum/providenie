59dabba008c7d9d1534781070c0ec72b82859792
Андрей Распутин <rasputin21@gmail.com>
2022-12-22 20:16:55 +0500

    run_polling()

--- src/bot/services.py
+++ src/bot/services.py
@@ -1,7 +1,7 @@
-from conversations.menu import start
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-from src.core.settings import TELEGRAM_TOKEN
+from bot.conversations.menu import start
+from core.settings import TELEGRAM_TOKEN
 
 
 def start_bot():
@@ -9,3 +9,4 @@ def start_bot():
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
+    return application
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import core.logger  # noqa
+from bot.services import start_bot
 
 
 def main() -> None:
-    pass
+    application = start_bot()
+    application.run_polling()
 
 
 if __name__ == "__main__":
