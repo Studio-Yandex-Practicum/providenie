5f36cce72d6976bebe7b0548a656c3d0d08dad85
Финатова Ирина <irinafinatova@yandex.ru>
2022-12-26 01:08:01 +0300

    Перенос handlers в файл handlers, перенос parents_chat в parents_chat.

---
+++ src/bot/handlers.py
@@ -0,0 +1,70 @@
+from telegram.ext import (
+    CallbackQueryHandler,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+
+(SELECTING_ACTION, CHAT, APPLICATION, VOLUNTEER,
+TALK, DONATION, EVENTS, QUESTION, ABOUT
+ ) = map(chr, range(9))
+
+(SELECTING_CHAT, CHAT_BABY, CHAT_CHILD,
+SHUNTATA, RETINOPATIA, GRANDMOTHERS, CRY,
+ANGELS, RETINOPATIA_4_5, PROBLEMS,
+TELECRAM_CHAT) = map(chr, range(9, 20))
+
+
+(PARENTS_SURNAME, TELEPHONE_NUMBER, BABY_SURNAME,
+DATE_OF_BIRTH, PLACE_OF_BIRTH, TERM_OF_BIRTH,
+WEIGHT, HEIGHT) = map(chr, range(21, 29))
+SELECTING_CHAT, TYPING = map(chr, range(29, 31))
+
+STOPPING, SHOWING = map(chr, range(31, 33))
+END = ConversationHandler.END
+(START_OVER, CURRENT_CHAT) = map(chr, range(19, 21))
+
+
+description_conv = ConversationHandler(
+    entry_points=[CallbackQueryHandler(
+        select_chat, pattern="^" + str(CHAT_BABY) + "$|^" + str(CHAT_CHILD) + "$|"
+        "^" + str(RETINOPATIA) + "$|^" + str(SHUNTATA) + "$|^" + str(GRANDMOTHERS) + "$|"
+        "^" + str(CRY) + "$|^" + str(ANGELS) + "$|^" + str(RETINOPATIA_4_5) + "$|"
+        "^" + str(PROBLEMS) + "$|^" + str(TELECRAM_CHAT) + "$")],
+    states={
+        PARENTS_SURNAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, parents_surname)],
+        TELEPHONE_NUMBER: [MessageHandler(filters.TEXT & ~filters.COMMAND, telephone_number)],
+        BABY_SURNAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, baby_surname)]},
+    fallbacks=[
+        CallbackQueryHandler(end_second_level, pattern="^" + str(END) + "$"),
+        CommandHandler("stop", stop_nested)],
+    map_to_parent={
+         END: SELECTING_CHAT,
+         STOPPING: STOPPING,
+         },
+)
+
+
+selection_handlers = [
+    description_conv,
+    CallbackQueryHandler(select_chat, pattern="^" + str(CHAT) + "$"),
+    CallbackQueryHandler(application, pattern="^" + str(APPLICATION) + "$"),
+    CallbackQueryHandler(become_volunteer, pattern="^" + str(VOLUNTEER) + "$"),
+    CallbackQueryHandler(talk_friends, pattern="^" + str(TALK) + "$"),
+    CallbackQueryHandler(give_donation, pattern="^" + str(DONATION) + "$"),
+    CallbackQueryHandler(get_events, pattern="^" + str(EVENTS) + "$"),
+    CallbackQueryHandler(ask_question, pattern="^" + str(QUESTION) + "$"),
+    CallbackQueryHandler(get_about_fond, pattern="^" + str(ABOUT) + "$"),
+    CallbackQueryHandler(end, pattern="^" + str(END) + "$")
+]
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler("start", start)],
+    states={
+        SHOWING: [CallbackQueryHandler(start, pattern="^" + str(END) + "$")],
+        SELECTING_ACTION: selection_handlers,
+        STOPPING: [CommandHandler("start", start)],
+        },
+    fallbacks=[CommandHandler("end", end)],
+    )
