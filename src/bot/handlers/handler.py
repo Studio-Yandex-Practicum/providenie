fd6a6d75de0ec11dee7fddb9f70ea59b3d708465
Владимир Рязанов <ryazanov745@gmail.com>
2023-02-05 09:03:35 +0300

    Вернул как все было раньше в файле

--- src/bot/handlers/handler.py
+++ src/bot/handlers/handler.py
@@ -22,10 +22,8 @@ selection_handlers = [
     CallbackQueryHandler(menu.get_events, pattern="^" + key.EVENTS + "$"),
     ask_question_conv,
     tell_about_fund_conv,
-    CallbackQueryHandler(
-        menu.end_second_level, pattern="^" + str(key.END) + "$"
-    ),
     CallbackQueryHandler(menu.about, pattern="^" + key.ABOUT + "$"),
+    CallbackQueryHandler(menu.end, pattern="^" + str(key.END) + "$"),
     CallbackQueryHandler(menu.end, pattern="^" + key.SENT + "$"),
 ]
 
