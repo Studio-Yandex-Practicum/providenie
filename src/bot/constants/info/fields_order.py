ec35933f169980f0d04665a8312cfd3ccd525ae9
Яснов Кирилл Сергеевич <YasnovKS@snc.local>
2023-03-27 10:20:52 +0500

    feature: add fields ordering in FundForm

---
+++ src/bot/constants/info/fields_order.py
@@ -0,0 +1,6 @@
+FUND_FIELDS_ORDER = ['parent_full_name', 'phone', 'email', 'social_networks',
+                     'child_full_name', 'child_birthday', 'family_members',
+                     'address', 'city', 'child_birth_place', 'child_birth_date',
+                     'child_birth_weight', 'child_birth_height', 'child_diagnosis',
+                     'parents_work_place', 'where_got_info', 'required_therapy',
+                     'request_goal', 'another_fund_member', ]
