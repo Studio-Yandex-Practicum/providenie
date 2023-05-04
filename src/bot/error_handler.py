fc0aa414746a9bd9aff47f20846f08f43e85481e
JMiki <themrjmiki@gmail.com>
2023-05-04 18:21:05 +0300

    fix: extra comma

--- src/bot/error_handler.py
+++ src/bot/error_handler.py
@@ -5,5 +5,5 @@ from telegram.ext import ContextTypes
 
 
 async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Log Error caused by Update.."""
+    """Log Error caused by Update."""
     logging.error(msg="Exception while handling an update:", exc_info=context.error)
