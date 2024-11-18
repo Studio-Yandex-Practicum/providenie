50c47ff78fa3f764c4052efc56dd2332977a74d2
Magomed Varaev <kalash9505@yandex.ru>
2024-11-18 21:41:20 +0300

    Одноконтейнерый синхронный запуск FastAPI и бота

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,12 @@ FROM python:3.12-bookworm
 
 WORKDIR /app
 
+# Копируем зависимости и устанавливаем их
 COPY requirements.txt .
-RUN pip3 install -r requirements.txt --no-cache-dir
+RUN pip3 install --no-cache-dir -r requirements.txt
 
+# Копируем исходники
 COPY src/ .
 
+# Команда для запуска
 CMD ["python", "main.py"]
--- src/main.py
+++ src/main.py
@@ -1,14 +1,34 @@
+import uvicorn
 from telegram import Update
-
-from bot.core import logger  # noqa
 from bot.services import init_bot
+from app.main import app  # Импорт FastAPI-приложения
+import multiprocessing
 
 
-def main() -> None:
-    """Run application."""
+def run_bot():
+    """Запуск Telegram бота."""
     application = init_bot()
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
-if __name__ == '__main__':
-    main()
+def run_fastapi():
+    """Запуск FastAPI."""
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+def run_both():
+    """Запуск Telegram бота и FastAPI в разных процессах."""
+    bot_process = multiprocessing.Process(target=run_bot)
+    fastapi_process = multiprocessing.Process(target=run_fastapi)
+
+    # Запускаем процессы
+    bot_process.start()
+    fastapi_process.start()
+
+    # Ждем завершения процессов
+    bot_process.join()
+    fastapi_process.join()
+
+
+if __name__ == "__main__":
+    run_both()
