f54ce7e3bf8bc6e98117140649e2e337ed26a21e
Magomed Varaev <kalash9505@yandex.ru>
2024-11-18 18:45:14 +0300

    Реализован одновременный запуск FastAPI и бота

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from typing import Dict
 
 app = FastAPI()
 
+
 @app.get("/")
-def read_root():
+def read_root() -> Dict[str, str]:
+    """
+    Возвращает приветственное сообщение в формате JSON.
+    """
     return {"message": "Здравствуйте, API работает!"}
