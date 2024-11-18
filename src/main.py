a25918db5c5380972ca6de7155d7dba6d1e4f6cc
Magomed Varaev <kalash9505@yandex.ru>
2024-11-19 00:12:10 +0300

    Одноконтейнерный синхронный запуск FastAPI и бота

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import multiprocessing
-import uvicorn
 
+import uvicorn
 from telegram import Update
 
 from app.main import app  # Импорт FastAPI-приложения
+
 from bot.services import init_bot
 
 
@@ -15,7 +16,7 @@ def run_bot() -> None:
 
 def run_fastapi() -> None:
     """Запуск FastAPI."""
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
 
 
 def run_both() -> None:
@@ -32,5 +33,5 @@ def run_both() -> None:
     fastapi_process.join()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run_both()
