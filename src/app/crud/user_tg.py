47a8fb2886c2d6a7b9fdd6a09c5fc697d956dd7e
Levchenko Aleksandr <madlion@mail.ru>
2024-11-21 13:33:11 +0700

    fix user_tg crud. Changes to be committed: 	modified:   src/app/crud/user_tg.py 	deleted:    src/app/endpoints/validators.py

--- src/app/crud/user_tg.py
+++ src/app/crud/user_tg.py
@@ -1,10 +1,11 @@
 from typing import TypeVar
 
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy import exists
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 from app.crud.base import CRUDBase
-from app.endpoints.validators import check_tg_id_unique
 from app.models.models import UserTG
 
 ModelType = TypeVar('ModelType')
@@ -19,8 +20,9 @@ class CRUDUserTG(CRUDBase):
             session: AsyncSession) -> ModelType:
         """Create new user in database."""
         new_user_dict = pydantic_scheme_user.dict()
-        await check_tg_id_unique(pydantic_scheme_user, session)
         password = new_user_dict.pop('password')
+        # TODO "заменить на получение хеша после создания функций
+        # для авторизации"
         new_user_dict['hashed_password'] = hash(password)
         new_user = self.model(**new_user_dict)
         session.add(new_user)
@@ -38,10 +40,10 @@ class CRUDUserTG(CRUDBase):
         update_data = pydantic_scheme_user.dict(
             exclude_unset=True,
             exclude_none=True)
-        if 'tg_id' in update_data:
-            await check_tg_id_unique(pydantic_scheme_user, session)
         if 'password' in update_data:
             password = update_data.pop('password')
+            # TODO "заменить на получение хеша после создания функций
+            # для авторизации"
             update_data['hashed_password'] = hash(password)
         for field in update_data:
             if hasattr(user_data, field):
@@ -50,6 +52,19 @@ class CRUDUserTG(CRUDBase):
         await session.commit()
         await session.refresh(db_user)
         return db_user
+
+    async def check_tg_id_unique(
+            self,
+            pydantic_scheme_user: ModelType,
+            session: AsyncSession) -> bool:
+        """Check unique telegram id for new user.
+
+        Returns True if there is no user with such a tg_id and False otherwise.
+        """
+        tg_id = pydantic_scheme_user.tg_id
+        user_exists = await session.execute(
+            select(exists().where(UserTG.tg_id == tg_id)))
+        return not user_exists.scalar()
 
 
 crud_user = CRUDUserTG(UserTG)
