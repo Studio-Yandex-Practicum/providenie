eb96e3ab2ce5a480d667e05ab4a04c780e721768
Александров Роман <rsateam@yandex.ru>
2024-12-05 08:50:58 +0300

    Update user_tg.py

--- src/app/crud/user_tg.py
+++ src/app/crud/user_tg.py
@@ -24,10 +24,9 @@ class CRUDUserTG(CRUDBase):
         """Create new user in database."""
         new_user_dict = pydantic_scheme_user.dict()
         password = new_user_dict.pop('password')
-        groups = new_user_dict.pop('groups', None)
+        groups = new_user_dict.pop('groups')
 
-        if password:
-            new_user_dict['hashed_password'] = get_hash_password(password)
+        new_user_dict['hashed_password'] = get_hash_password(password)
         new_user: UserTG = self.model(**new_user_dict)
         session.add(new_user)
         await session.commit()
@@ -41,12 +40,12 @@ class CRUDUserTG(CRUDBase):
             )
             group_objects = result.scalars().all()
 
-            for group in group_objects:
-                association = UserGroupAssociation(
-                    user_id=new_user.id,
-                    group_id=group.id,
-                )
-                session.add(association)
+        for group in group_objects:
+            association = UserGroupAssociation(
+                user_id=new_user.id,
+                group_id=group.id,
+            )
+            session.add(association)
 
         await session.commit()
         return new_user
